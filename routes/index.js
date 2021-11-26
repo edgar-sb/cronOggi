d01277bfd6b1648f9924ec29ed4edf7217d0543d
edyMartin <evalerio.sdc@hotmail.com>
2021-11-26 11:30:32 -0600

    okey

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var fs = require('fs')
 router.post('/', function (req, res, next) {
   let order = req.headers;
   writer({ "headers": JSON.stringify(order), "reqs": JSON.stringify(req), "res": JSON.stringify(res) });
-  
+
   var config = {
     method: 'get',
     url: 'http://54.227.217.31:3000/cartaporteById?id=1179130486445-01',
@@ -23,26 +23,26 @@ router.post('/', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-    read(res)
+  read(res)
 });
 
-const read = (res)=>{
+const read = (res) => {
   fs.readFile('2pac.txt', 'utf-8', (err, data) => {
-    if(err) {
-      res.json({Error: err})
+    if (err) {
+      res.json({ Error: err })
     } else {
-      res.json({archivo: data})
+      res.json({ archivo: data })
     }
   });
 }
 
-const writer = (data)=>{
+const writer = (data) => {
   fs.writeFile('2pac.txt', JSON.stringify(data), (err) => {
     // throws an error, you could also catch it here
     if (err) throw err;
 
     // success case, the file was saved
     console.log('Lyric saved!');
-});
-
-module.exports = router;
+  });
+}
+module.exports = router
