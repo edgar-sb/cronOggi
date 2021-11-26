4a44dd91c57bbb65b35ce0ed925ce92f257a634f
edyMartin <evalerio.sdc@hotmail.com>
2021-11-26 10:32:54 -0600

    master

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+/* GET home page. */
+router.get('/hookogg', function (req, res, next) {
+  let order = req.headers['order_id'];
+  var config = {
+    method: 'get',
+    url: 'http://${ip}:3000/cartaporteById?id=1175302482041-01',
+    headers: {}
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json({ "message": "Hello" })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+ 
+});
+
+router.get('/', function (req, res, next) {
+  res.json({ "message": "Hello" })
+});
+module.exports = router;
