ec8a866d42301b8eb67cce5018839161b17c1006
edyMartin <evalerio.sdc@hotmail.com>
2021-11-26 11:22:20 -0600

    okey

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
-
+var fs = require('fs')
 /* GET home page. */
 router.post('/hookogg', function (req, res, next) {
-  let order = req.headers['order_id'];
+  let order = req.headers;
+  writer({ "headers": JSON.stringify(order), "reqs": JSON.stringify(req), "res": JSON.stringify(res) });
+  
   var config = {
     method: 'get',
     url: 'http://54.227.217.31:3000/cartaporteById?id=1179130486445-01',
@@ -19,10 +21,22 @@ router.post('/hookogg', function (req, res, next) {
       console.log(error);
     });
 
- 
+
 });
 
 router.get('/', function (req, res, next) {
-  res.json({ "message": "Hello" })
+    read(res)
 });
+
+const read = (res)=>{
+  fs.readFile('2pac.txt', 'utf-8', (err, data) => {
+    if(err) {
+      res.json({Error: err})
+    } else {
+      res.json({archivo: data})
+    }
+  });
+}
+
+
 module.exports = router;
